732945167297589493b17410cf6082eebf68e75e
Anesa Kršlak <akrslak17@gmail.com>
2024-11-18 20:36:01 +0100

    Delete app directory

--- app/src/main/java/ba/etf/rma22/projekat/data/models/Anketa.kt
+++
@@ -1,8 +0,0 @@
-package ba.etf.rma22.projekat.data.models
-
-import java.util.*
-
-data class Anketa(
-    val naziv: String, val nazivIstrazivanja: String, val datumPocetka: Date, val datumKraj: Date,
-    val datumRada: Date?, val trajanje: Int, val nazivGrupe: String, val progres: Float
-)
--- app/src/main/java/ba/etf/rma22/projekat/data/models/Istrazivanja.kt
+++
@@ -1,3 +0,0 @@
-package ba.etf.rma22.projekat.data.models
-
-data class Istrazivanja(val naziv: String, val godina: Int)
